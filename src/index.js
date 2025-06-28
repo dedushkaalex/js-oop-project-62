bd6d5ff3f4de92e7cadbe73dd5a52c9205c70663
Aleksei Erchenko <erchenkoaleksei@yandex.ru>
2025-06-28 16:15:12 +0300

    fix: ci

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import Validator from './Validator.js';
-
-const v = new Validator();
-
-const schema = v.object();
-
-// Позволяет описывать валидацию для свойств объекта
-schema.shape({
-  name: v.string().required(),
-  age: v.number().positive(),
-});
-
-console.log(schema.isValid({ name: 'kolya', age: 100 })); // true
-console.log(schema.isValid({ name: 'maya', age: null })); // true
-console.log(schema.isValid({ name: '', age: null })); // false
-console.log(schema.isValid({ name: 'ada', age: -5 })); // false
+import Validator from './validator.js'
